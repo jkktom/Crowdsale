b5309e78821841aeea5fcb782621e58bd3829d5b
jk <jkt@jkui-iMac.local>
2023-03-23 23:30:29 +0900

    Migration done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  //deploy Token
+  const NAME = 'Dapp University'
+  const SYMBOL = 'DAPP'
+  const MAX_SUPPLY = '1000000'
+  const PRICE = ethers.utils.parseUnits('0.025', 'ether')
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // Deploy Token
+  const Token = await hre.ethers.getContractFactory("Token")
+  const token = await Token.deploy(NAME, SYMBOL, MAX_SUPPLY)
+  await token.deployed()
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(`Token deployed to: ${token.address}\n`)
 
-  await lock.deployed();
+  // Deploy Crowdsale
+  const Crowdsale = await hre.ethers.getContractFactory("Crowdsale")
+  const crowdsale = await Crowdsale.deploy(token.address, PRICE, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await crowdsale.deployed();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Crowdsale deployed to: ${crowdsale.address}\n`)
+
+  const transaction = await token.transfer(crowdsale.address, ethers.utils.parseUnits(MAX_SUPPLY, 'ether'))
+  await transaction.wait()
+
+  console.log(`Tokens transferred to Crowdsale\n`)
+
+
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
