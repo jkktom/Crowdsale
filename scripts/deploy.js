2b8ddf6e2e9e2fe8b7f09c40487f14d631d05e10
jk <jkt@jkui-iMac.local>
2023-03-24 00:43:16 +0900

    Deployment Done - again

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,12 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  //deploy Token
   const NAME = 'Dapp University'
   const SYMBOL = 'DAPP'
   const MAX_SUPPLY = '1000000'
@@ -25,9 +30,10 @@ async function main() {
   await transaction.wait()
 
   console.log(`Tokens transferred to Crowdsale\n`)
-
-
 }
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
